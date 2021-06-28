ac7cd0d501ae72a051a92efd10b5524c331f5192
om2009 <80381254+om2009@users.noreply.github.com>
2021-06-28 04:53:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ htmldata = "<section class='cards'>"
 menu_list_array.sort
 for (var k=0; k<menu_list_array.length; k++)
 {
-    htmldata = htmldata + "<div class='cards'>" + '<img class="img" src= "images/pizzaImg.png"/>' + menu_list_array[k] + "</div>";
+    htmldata = htmldata + "<div class='cards'>" + '<img class="img" src= "pizzaImg.png"/>' + menu_list_array[k] + "</div>";
 }
 htmldata = htmldata + "</section>"
 document.getElementById("display_addedmenu").innerHTML = htmldata;
