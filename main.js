ed7d0b60cebe0c7566b090a2991d74e147fc5ef0
om2009 <80381254+om2009@users.noreply.github.com>
2021-08-03 22:47:50 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,92 +1,129 @@
-// Create a reference for the canvas
-canvas = document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
+canvas = new fabric.Canvas("myCanvas");
 
-img_width = 300;
-img_height = 100;
+// Create canvas variable
 
-var img_image;
+//Set initial positions for ball and hole images.
+ball_x = 0;
+ball_y = 0;
+hole_x = 800;
+hole_y = 400;
 
-img_x = 100;
-img_y = 100;
+block_image_width = 5;
+block_image_height = 5;
 
-function add() {
-	img_imgTag = new Image(); //defining a variable with a new image
-	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
-	img_imgTag.src = img_image;   // load image
+function load_img(){
+	// write code to Upload golf image on the canvas
+	fabric.Image.fromURL("golf-h.png", function(Img){
+		hole_obj = Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(hole_obj);
+	});
+	new_image();
 }
 
-function uploadimg() {
-
-	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
-}
-function aplhabetkey()
+function new_image()
 {
-	//upload respective image with the message.
-	img_image = "Alpkey.png";
-	add(); 
-
-}
-function numberkey()
-{
-	img_image = "numkey.png";
-	add();	
-}
-function arrowkey()
-{
-	img_image = "Arrkey.png";
-	add();
-}
-function specialkey()
-{
-	img_image = "spkey.png";
-	add();
-	
-}
-function otherkey()
-{
-	img_image="otherkey.png";
-	add();
+	// write code to Upload ball image on canvas
+	fabric.Image.fromURL("ball.png", function(Img){
+		ball_obj = Img;
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:ball_y,
+			left:ball_x
+		});
+		canvas.add(ball_obj);
+	});
 }
 
-//Write a code to grab the key-pressed event
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	
-		if((keyPressed >='97' && keyPressed<='122')|| (keyPressed >='65' && keyPressed<='90'))
-		{
-			aplhabetkey();
-			console.log("aplhabet key");
-			document.getElementById("d1").innerHTML = "you have preesed aplhabet key";
-		}
-		else if (keyPressed >= '48' && keyPressed<='57')
-		{
-			numberkey();
-			console.log("number key");
-			document.getElementById("d1").innerHTML = "you have preesed number key";
-		}
-		else if (keyPressed >= '37' && keyPressed<='40')
-		{
-			arrowkey();
-			console.log("arrow key");
-			document.getElementById("d1").innerHTML = "you have preesed arrow key";
-		}
-		else if ((keyPressed == '17' ) && (keyPressed == '18') && (keyPressed == '27'))
-		{
-			specialkey();
-			console.log("special key");
-			document.getElementById("d1").innerHTML = "you have preesed special key";
-		}
-		//write a code to check the type of key pressed
-	else
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	if((ball_x==hole_x)&&(ball_y==hole_y))
 	{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+		canvas.remove(ball_obj);
+		document.getElementById("hd3").innerHTML ="you won";
+		document.getElementById("myCanvas").style.borderColor ="red";
 	}
-}
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+		if(ball_y>=5)
+		{
+			ball_y = ball_y-block_image_height;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
 
+	function down()
+	{
+		 // Write a code to move ball downward.
+		 if(ball_y<=450)
+		{
+			ball_y = ball_y+block_image_height;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+			ball_x = ball_x-block_image_width;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+			ball_x = ball_x+block_image_width;
+			canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+	
+}
 
