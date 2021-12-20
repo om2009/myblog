90bd1c43a0811a4696b7f0067dc0d1637b6986e6
om2009 <80381254+om2009@users.noreply.github.com>
2021-12-20 12:09:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,70 @@
-function laptop()
+status_lable = "";
+object = [];
+percent = "";
+song = "";
+function preload()
 {
-    window.open("laptop.html")
+    song = loadSound("alarm.mp3");
+    
 }
-function ac()
+
+function setup()
 {
-    window.open("ac.html")
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cococssd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting object";
+
 }
-function bottel()
+
+function modelLoaded()
 {
-    window.open("bottel.html");
+    console.log("model is ready");
+    status_lable = true;
+    objectDetector.detect(video, gotResult);
 }
-function mobile()
+
+
+function gotResult(error, result)
 {
-    window.open("mobile.html")
+    if(error)
+    {
+        console.error(error);
+
+    }else{
+        console.log(result);
+        object = result;
+    }
 }
+
+function draw()
+{
+    image(video,0,0,380, 380);
+    if(status_lable != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of detected objects : " + object.length;
+            fill("red");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+"%", object[i].x+15, object[i].y+15);
+            noFill();
+            stroke("red");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+            if(object[i].label == 'person')
+            {
+                song.stop();
+            }else{
+                song.play();
+            }
+        }
+    }
+    
+}
+
+
